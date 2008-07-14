897675561e47e7a0757da027fa6381f51eac5941
Justin Karneges <justin@affinix.com>
2008-07-14 23:58:00 +0000

    update copyright
    git-svn-id: http://delta.affinix.com/svn/trunk/iris@143 ac7a513c-5e3a-0410-af42-968c3ac6e7dc

--- src/irisnet/appledns/qdnssd.cpp
+++ src/irisnet/appledns/qdnssd.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007  Justin Karneges
+ * Copyright (C) 2007,2008  Justin Karneges
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
