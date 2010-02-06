6b7128c76ea9b9d87604e100fbf71a7d9d9d888f
Justin Karneges <justin@affinix.com>
2010-02-06 20:22:49 +0000

    fix compilation when XMPP_DEBUG is used
    git-svn-id: http://delta.affinix.com/svn/trunk/iris@772 ac7a513c-5e3a-0410-af42-968c3ac6e7dc

--- src/xmpp/xmpp-core/connector.cpp
+++ src/xmpp/xmpp-core/connector.cpp
@@ -494,7 +494,7 @@ void AdvancedConnector::do_connect()
 	d->connectTimeout.start(5000);
 
 #ifdef XMPP_DEBUG
-	printf("trying %s:%d\n", d->host.latin1(), d->port);
+	printf("trying %s:%d\n", qPrintable(d->host), d->port);
 #endif
 	int t = d->proxy.type();
 	if(t == Proxy::None) {
