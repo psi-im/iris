c8b867d610dc8641247202319dcb068a497648d2
Sergey Ilinykh <rion4ik@gmail.com>
2018-12-05 00:37:04 +0300

    ServerInfoManager moved to iris

---
+++ src/xmpp/xmpp-im/xmpp_serverinfomanager.cpp
@@ -0,0 +1,95 @@
+/*
+ * xmpp_serverinfomanager.cpp
+ * Copyright (C) 2006  Remko Troncon
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+#include "xmpp_serverinfomanager.h"
+#include "xmpp_tasks.h"
+#include "xmpp_caps.h"
+
+namespace XMPP {
+
+ServerInfoManager::ServerInfoManager(Client* client)
+    : _client(client)
+    , _canMessageCarbons(false)
+{
+    deinitialize();
+    connect(_client, SIGNAL(rosterRequestFinished(bool, int, const QString &)), SLOT(initialize()), Qt::QueuedConnection);
+}
+
+void ServerInfoManager::reset()
+{
+    _hasPEP = false;
+    _multicastService = QString();
+    disconnect(CapsRegistry::instance());
+    disconnect(_client, SIGNAL(disconnected()), this, SLOT(deinitialize()));
+}
+
+void ServerInfoManager::initialize()
+{
+    connect(_client, SIGNAL(disconnected()), SLOT(deinitialize()));
+    JT_DiscoInfo *jt = new JT_DiscoInfo(_client->rootTask());
+    connect(jt, SIGNAL(finished()), SLOT(disco_finished()));
+    jt->get(_client->jid().domain());
+    jt->go(true);
+}
+
+void ServerInfoManager::deinitialize()
+{
+    reset();
+    emit featuresChanged();
+}
+
+const QString& ServerInfoManager::multicastService() const
+{
+    return _multicastService;
+}
+
+bool ServerInfoManager::hasPEP() const
+{
+    return _hasPEP;
+}
+
+bool ServerInfoManager::canMessageCarbons() const
+{
+    return _canMessageCarbons;
+}
+
+void ServerInfoManager::disco_finished()
+{
+    JT_DiscoInfo *jt = static_cast<JT_DiscoInfo *>(sender());
+    if (jt->success()) {
+        _features = jt->item().features();
+
+        if (_features.hasMulticast())
+            _multicastService = _client->jid().domain();
+
+        _canMessageCarbons = _features.hasMessageCarbons();
+
+        // Identities
+        DiscoItem::Identities is = jt->item().identities();
+        foreach(DiscoItem::Identity i, is) {
+            if (i.category == "pubsub" && i.type == "pep")
+                _hasPEP = true;
+        }
+
+        emit featuresChanged();
+    }
+}
+
+} // namespace XMPP
---
+++ src/xmpp/xmpp-im/xmpp_serverinfomanager.h
@@ -0,0 +1,69 @@
+/*
+ * xmpp_serverinfomanager.h
+ * Copyright (C) 2006  Remko Troncon
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+#ifndef SERVERINFOMANAGER_H
+#define SERVERINFOMANAGER_H
+
+#include "xmpp_caps.h"
+
+#include <QObject>
+#include <QString>
+
+namespace XMPP {
+
+class Client;
+class Features;
+class Jid;
+class DiscoItem;
+
+class ServerInfoManager : public QObject
+{
+    Q_OBJECT
+
+public:
+    ServerInfoManager(XMPP::Client* client);
+
+    const QString& multicastService() const;
+    bool hasPEP() const;
+    inline const Features &features() const { return _features; }
+    bool canMessageCarbons() const;
+
+signals:
+    void featuresChanged();
+
+private slots:
+    void disco_finished();
+    void initialize();
+    void deinitialize();
+    void reset();
+
+private:
+    XMPP::Client* _client = nullptr;
+    CapsSpec _caps;
+    Features _features;
+    QString _multicastService;
+    bool _featuresRequested;
+    bool _hasPEP;
+    bool _canMessageCarbons;
+};
+
+} // namespace XMPP
+
+#endif
