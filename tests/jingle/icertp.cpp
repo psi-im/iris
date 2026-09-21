3098b4c6701cc9fecc3fcb059384665e7d643e6c
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-21 23:36:19 +0300

    Include DTLS API in ICE RTP regression

--- tests/jingle/icertp.cpp
+++ tests/jingle/icertp.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 #include "../../src/xmpp/xmpp-im/jingle-ice-connection_p.h"
 #include <iris/ice176.h>
+#include <iris/dtls.h>
 #include <iris/jingle-rtp-srtp.h>
 #include <iris/jingle-rtp.h>
 #include <iris/jingle-session.h>
